3309b9da2e1806c1b3b2ca70b5323e5ebf9acde9
Adrian <adrian.madalin.dinu@gmail.com>
2018-01-12 11:23:38 +0200

    added and configured sockedt.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const port = process.env.PORT || 8080;
-
 const publicPath = path.join(__dirname, '../public');
+
+let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`App started on port ${port}`);
 });
+
